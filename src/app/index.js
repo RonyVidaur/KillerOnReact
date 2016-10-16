842d78376c04ea941c535849a88dc2629cd6bdd6
RonyVidaur <rony.vidaur@gmail.com>
2016-10-16 09:18:48 -0600

    added props

--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,18 @@ var ReactDom = require('react-dom');
 //create component
 var TodoComponent = React.createClass({
   render: function(){
-    return (
-      <h1>helloooooo</h1>);
+    return(
+      <div>
+          <h1>Hello <strong>{this.props.person.name}!</strong></h1>
+          <p>I see that you like {this.props.person.hobby}, that is so cool!</p>
+          <p>and {this.props.person.favoriteTeam} is a very strong choice aswell</p>
+      </div>
+    );
   }
 });
 
+var person = {name:"Rony Vidaur",
+              hobby:"Coding & Soccer",
+              favoriteTeam:"Real Madrid"}
 //put the component into the page
-ReactDom.render(<TodoComponent/>,document.getElementById('todo-wrapper'));
+ReactDom.render(<TodoComponent person = {person} />,document.getElementById('todo-wrapper'));
