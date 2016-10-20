9fc4dd9ca4f93dc9430d773fc93a38996f6961b3
RonyVidaur <rony.vidaur@gmail.com>
2016-10-20 09:44:03 -0600

    state added

--- src/app/index.js
+++ src/app/index.js
@@ -3,19 +3,33 @@ var ReactDom = require('react-dom');
 
 //create component
 var TodoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: ["eat", "work", "sleep"]
+    }
+  },
   render: function(){
     return(
       <div>
           <h1>Hello <strong>{this.props.person.name}!</strong></h1>
           <p>I see that you like {this.props.person.hobby}, that is so cool!</p>
           <p>and {this.props.person.favoriteTeam} is a very strong choice aswell</p>
+          <div id="todo-list">
+            <p>Here is today's schedule</p>
+            <ul>
+              <li>{this.state.todos[0]}</li>
+              <li>{this.state.todos[1]}</li>
+              <li>{this.state.todos[2]}</li>
+            </ul>
+          </div>
       </div>
+
     );
-  }
+  }//end of render
 });
 
 var person = {name:"Rony Vidaur",
               hobby:"Coding & Soccer",
               favoriteTeam:"Real Madrid"}
 //put the component into the page
-ReactDom.render(<TodoComponent person = {person} />,document.getElementById('todo-wrapper'));
+ReactDom.render(<TodoComponent person = {person} />, document.getElementById('todo-wrapper'));
