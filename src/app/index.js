88e4319fa3beda9b82c5aa39b2f42af87818c451
RonyVidaur <rony.vidaur@gmail.com>
2016-11-06 22:58:58 -0600

    handling click event

--- src/app/index.js
+++ src/app/index.js
@@ -9,19 +9,28 @@ var TodoComponent = React.createClass({
     }
   },
   render: function(){
-    var todos = this.state.todos.map(function(item,index){
+    var todos = this.state.todos
+      todos = todos.map(function(item,index){
       return (
-        <TodoItem item = {item} key={index}/>
+        <TodoItem item={item} key={index} onDelete={this.onDelete}/>
       )
-    })
+    }.bind(this))
     return(
       <div>
-        <h1>Hello here is what You need to complete</h1>
+        <h1 >Hello here is what You need to complete</h1>
         <ul>{todos}</ul>
       </div>
 
     );
-  }//end of render
+  },//end of render
+  //custom functions
+  onDelete: function(item){
+    var updatedTodos = this.state.todos.filter(function(val, index){
+      return item !== val;
+    })
+    this.setState({todos: updatedTodos
+    })
+  }
 });
 
 var TodoItem = React.createClass({
@@ -30,9 +39,15 @@ var TodoItem = React.createClass({
       <li>
         <div className="todo-item">
           <span className="item-name">{this.props.item}</span>
+          <span className="item-delete" onClick={this.handleDelete}> x </span>
         </div>
+
       </li>
     )
+  },
+  //custom functions
+  handleDelete: function(){
+    this.props.onDelete(this.props.item)
   }
 })
 
