03fd14925bf285661b4b1ebafb23ea0226b9073b
RonyVidaur <rony.vidaur@gmail.com>
2016-11-06 21:08:44 -0600

    nesting components

--- src/app/index.js
+++ src/app/index.js
@@ -5,30 +5,40 @@ var ReactDom = require('react-dom');
 var TodoComponent = React.createClass({
   getInitialState: function(){
     return {
-      todos: ["eat", "work", "sleep", "greet gf"]
+      todos: ["eat", "work", "sleep", "gf"]
     }
   },
   render: function(){
     var todos = this.state.todos.map(function(item,index){
-      return (<li>{item}</li>)
+      return (
+        <TodoItem item = {item} key={index}/>
+      )
     })
     return(
       <div>
-          <h1>Hello <strong>{this.props.person.name}!</strong></h1>
-          <p>I see that you like {this.props.person.hobby}, that is so cool!</p>
-          <p>and {this.props.person.favoriteTeam} is a very strong choice aswell</p>
-          <div id="todo-list">
-            <p>Here is today's schedule</p>
-            <ul>{todos}</ul>
-          </div>
+        <h1>Hello here is what You need to complete</h1>
+        <ul>{todos}</ul>
       </div>
 
     );
   }//end of render
 });
 
+var TodoItem = React.createClass({
+  render: function(){
+    return(
+      <li>
+        <div className="todo-item">
+          <span className="item-name">{this.props.item}</span>
+        </div>
+      </li>
+    )
+  }
+})
+
 var person = {name:"Rony Vidaur",
               hobby:"Coding & Soccer",
               favoriteTeam:"Real Madrid"}
+
 //put the component into the page
 ReactDom.render(<TodoComponent person = {person} />, document.getElementById('todo-wrapper'));
