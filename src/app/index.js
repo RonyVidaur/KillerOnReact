7c430b5e8914250ae62be2df576199ee13a4eda1
RonyVidaur <rony.vidaur@gmail.com>
2016-11-07 11:50:59 -0600

    add-item component is now working

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 var TodoItem = require('./todo-item')
+var AddItem = require('./add-item')
 require('./css/index.css')
 //create component
 var TodoComponent = React.createClass({
@@ -19,6 +20,7 @@ var TodoComponent = React.createClass({
         <h1 >Hello, {this.props.person}</h1>
         <p>remember to get shit done</p>
         <ul>{todos}</ul>
+        <AddItem onAdd={this.onAdd}/>
       </div>
     );
   },//end of render
@@ -29,6 +31,11 @@ var TodoComponent = React.createClass({
     })
     this.setState({todos: updatedTodos
     })
+  },
+  onAdd: function(item){
+    var updatedTodos = this.state.todos
+    updatedTodos.push(item)
+    this.setState({todos: updatedTodos})
   }
 });
 //put the component into the page
