1737e96323acdcfe25e4967efe24229c4744b5d1
RonyVidaur <rony.vidaur@gmail.com>
2016-11-07 10:54:23 -0600

    styles

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-
+var TodoItem = require('./todo-item')
+require('./css/index.css')
 //create component
 var TodoComponent = React.createClass({
   getInitialState: function(){
@@ -11,49 +12,24 @@ var TodoComponent = React.createClass({
   render: function(){
     var todos = this.state.todos
       todos = todos.map(function(item,index){
-      return (
-        <TodoItem item={item} key={index} onDelete={this.onDelete}/>
-      )
+      return (<TodoItem item={item} key={index} onDelete={this.onDelete}/>)
     }.bind(this))
     return(
       <div>
-        <h1 >Hello here is what You need to complete</h1>
+        <h1 >Hello, {this.props.person}</h1>
+        <p>remember to get shit done</p>
         <ul>{todos}</ul>
       </div>
-
     );
   },//end of render
   //custom functions
   onDelete: function(item){
     var updatedTodos = this.state.todos.filter(function(val, index){
-      return item !== val;
+      return item !== val
     })
     this.setState({todos: updatedTodos
     })
   }
 });
-
-var TodoItem = React.createClass({
-  render: function(){
-    return(
-      <li>
-        <div className="todo-item">
-          <span className="item-name">{this.props.item}</span>
-          <span className="item-delete" onClick={this.handleDelete}> x </span>
-        </div>
-
-      </li>
-    )
-  },
-  //custom functions
-  handleDelete: function(){
-    this.props.onDelete(this.props.item)
-  }
-})
-
-var person = {name:"Rony Vidaur",
-              hobby:"Coding & Soccer",
-              favoriteTeam:"Real Madrid"}
-
 //put the component into the page
-ReactDom.render(<TodoComponent person = {person} />, document.getElementById('todo-wrapper'));
+ReactDom.render(<TodoComponent person = "Rony" />, document.getElementById('todo-wrapper'));
